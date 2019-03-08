5e344f4933eb599ad0bfe7575b1e3655deeeeec6
keyflight <olivier@andev.nl>
2019-03-08 12:05:18 +0100

    Update styles & fix peer dependencies

--- src/contexts/recordDetail/editor.js
+++ src/contexts/recordDetail/editor.js
@@ -1,12 +1,45 @@
 import React from 'react'
 import {css} from 'emotion'
+import colors from '@cmds/colors'
+import Button from '@cmds/button'
 import OptionList from '@cmds/select/es/OptionList'
 import Option from '../../Option'
+
+
 
 export default class MultipleSelectField extends React.Component {
 
     state = {
         open: false
+    }
+
+    optionRenderer = ({option}) => {
+
+        const color = colors[option.colorId] || defaultColor
+
+        return (
+            <div
+                className={css`
+                    background-color: ${color.backgroundColor};
+                    color: ${color.color};
+                    min-width: 18px;
+                    height: 18px;
+                    font-size: 13px;
+                    font-weight: 400;
+                    max-width: 100%;
+                    align-items: center;
+                    display: inline-flex;
+                    padding-left: 8px;
+                    padding-right: 8px;
+                    line-height: 1.5;
+                    -webkit-print-color-adjust: exact;
+                    border-radius: 9999px;
+                    flex: 0 0 auto;
+                `}
+            >
+                {option.name}
+            </div>
+        )
     }
 
     render() {
@@ -32,30 +65,19 @@ export default class MultipleSelectField extends React.Component {
                             position: relative;
                         `}
                     >
-                        <button
-                            type={'button'}
-                            className={css`
-                                padding-top: 4px;
-                                padding-bottom: 4px;
-                                padding-left: 16px;
-                                padding-right: 16px;
-                                border-radius: 3px;
-                                background-color: hsla(0,0%,0%,0.05);
-                                font-weight: 500;
-                                color: #111;
-                                border: none;
-                            `}
+                        <Button
                             onClick={() => this.setState({open: true})}
                         >
                             Select an option
-                        </button>
+                        </Button>
                         {this.state.open ? (
                             <OptionList
                                 className={css`
-                                margin-top: -21px;
-                            `}
+                                    margin-top: -21px;
+                                `}
                                 alignLeft={true}
                                 options={options}
+                                optionRenderer={this.optionRenderer}
                                 onOptionClick={this.handleOptionLink}
                                 onClickOutside={() => this.setState({open: false})}
                             />
